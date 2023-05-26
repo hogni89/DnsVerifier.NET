b1dc75f3d6b374260ec8caa2b48e6546376779db
hogni89 <hognibeinisson@gmail.com>
2023-05-26 09:20:16 +0100

    Added support for MX records

---
+++ DnsVerifierNet/DnsVerifierNet/DnsRecords/MxDnsRecord.cs
@@ -0,0 +1,28 @@
+﻿using DnsVerifierNet.Records;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnsVerifierNet.DnsRecords
+{
+    public class MxDnsRecord : DnsRecord
+    {
+        public int Priority { get; set; }
+        public string Hostname { get; set; }
+        public string IpAddress { get; set; }
+        public MxDnsRecord(string type, string domainName, int ttl, int priority, string hostname, string ipAddress) : base(type, domainName, ttl)
+        {
+            Priority = priority;
+            Hostname = hostname;
+            IpAddress = ipAddress;
+        }
+
+        public override string ToString()
+        {
+            var result = base.ToString() + ", priority: " + Priority.ToString() + ", hostname: " + Hostname + ", ip address: " + IpAddress;
+            return result;
+        }
+    }
+}
--- DnsVerifierNet/DnsVerifierNet/Program.cs
+++ DnsVerifierNet/DnsVerifierNet/Program.cs
@@ -4,6 +4,8 @@ using DnsVerifierNet.DnsRecords;
 
 var aRecord = new ADnsRecord("A", "google.com", 300, "142.251.41.14");
 var txtRecord = new TxtDnsRecord("TXT", "google.com", 300, "This is a text record");
+var MxRecord = new MxDnsRecord("MX", "google.com", 300, 10, "smtp.google.com", "172.253.115.26");
 
 Console.WriteLine(aRecord.ToString());
 Console.WriteLine(txtRecord.ToString());
+Console.WriteLine(MxRecord.ToString());
